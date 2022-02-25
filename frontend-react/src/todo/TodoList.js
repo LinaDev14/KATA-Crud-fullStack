0f35ccb28613d6003ede8e37e75557baa963963c
LMFront-end <perfilgitlinamaria@gmail.com>
2022-02-25 16:23:37 -0500

    se agrega el metodo onEditcomplete

--- frontend-react/src/todo/TodoList.js
+++ frontend-react/src/todo/TodoList.js
@@ -23,12 +23,42 @@ const TodoList = (TaskListId) => {
     }, [dispatch]);
 
     const onDelete = (id) => {
-        fetch(URL_API + "/" + delete + id, {
+        fetch(URL_API + "/delete/" + id, {
             method: "DELETE"
         })
             .then((todo) => {
                 dispatch ({
                     type: "delete-item", id
+                })
+            })
+    }
+
+    const onEdit = (todo) => {
+        dispatch({
+            type: "edit-item",
+            item: todo
+        })
+    }
+
+    const onEditComplete = (event, item) => {
+        const request = {
+            name: item.name,
+            id: item.id,
+            idList: item.idList,
+            completed: event.target.checked
+        }
+
+        fetch(URL_API + "/todo/update", {
+            method: "PUT",
+            body: JSON.stringify(request),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(response => response.json())
+            .then((todo) => {
+                dispatch({
+                    type: "update-item", item: todo
                 })
             })
     }
