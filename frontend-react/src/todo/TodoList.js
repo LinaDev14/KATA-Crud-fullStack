bbb13455a00912bcd85c10066c9e7724644e3168
LMFront-end <perfilgitlinamaria@gmail.com>
2022-02-25 16:01:28 -0500

    se crea nuevo archivo TodoList

---
+++ frontend-react/src/todo/TodoList.js
@@ -0,0 +1,27 @@
+import React, {useContext, useEffect} from "react";
+
+// import components
+import {URL_API} from '../utils/Data';
+import {Store} from '../utils/initialState';
+
+const TodoList = (TaskListId) => {
+    const {dispatch, state: {
+        todo
+    }} = useContext(Store);
+    const currentList = todo.todoList.filter(todo => {
+        return todo.idList === TaskListId.TaskListId;
+    });
+
+    useEffect(() => {
+        fetch(URL_API + "/todos")
+            .then(response => response.json())
+            .then((todos) =>{
+                dispatch({
+                    type: "update-list", todos
+                })
+            })
+    }, [dispatch])
+
+}
+
+export {TodoList}
