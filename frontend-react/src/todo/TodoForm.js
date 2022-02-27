a48b8e217974fd519ebde94d020cf76bc529baa0
LMFront-end <perfilgitlinamaria@gmail.com>
2022-02-26 19:33:19 -0500

    se agrega un typing al title form

--- frontend-react/src/todo/TodoForm.js
+++ frontend-react/src/todo/TodoForm.js
@@ -4,6 +4,7 @@ import { useForm } from "react-hook-form";
 import styles from './TodoForm.module.css';
 // import image
 import task from '../images/task.svg';
+import Alert from '@mui/material/Alert'
 
 const TodoForm = () => {
     const { register, handleSubmit, formState: { errors }} = useForm();
@@ -17,7 +18,7 @@ const TodoForm = () => {
     return (
         <div className={styles.search__container}>
             <h1 className={styles.search__title}>
-                LinaDev <span>Todo App </span>
+                 <span className={styles.title}>Todo App </span>
             </h1>
             <img className={styles.search__img} src={task} alt="imagen gifs" />
             <form onSubmit={handleSubmit(onSubmit)} className={styles.search__user}>
@@ -31,11 +32,12 @@ const TodoForm = () => {
                     })}
                 />
                 <div>
-                <span className="text-danger text-small d-block mb-2">
-        {errors.todo?.type === 'required' && "The todo is required"}
-        </span>
+                <span>
+                {errors.todo?.type === 'required' && <Alert severity="error">"The todo is required"</Alert>}
+                </span>
                 </div>
                 <button className={styles.buttom_input}>Crear</button>
+
             </form>
 
         </div>
